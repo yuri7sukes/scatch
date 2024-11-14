612d8b1915382a19433fe06df62ad2c7a0ab7580
sykes <kanekikenrus@mail.ru>
2024-11-14 15:48:29 +0500

    change slider

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ for (let e of document.querySelectorAll('input[type="range"].slider-progress')) 
     e.style.setProperty('--max', e.max == '' ? '100' : e.max);
     e.addEventListener('input', () => e.style.setProperty('--value', e.value));
 }
+getCanvas(50, 50);
+
+const slider = document.querySelector('#myRange');
+slider.addEventListener("mouseup", () => {
+    let val = slider.value;
+    getCanvas(val, val)
+});
+
+slider.oninput = () => {
+    const gridSizeText = document.querySelector('.hello-text');
+    gridSizeText.textContent = `${slider.value}x${slider.value}`
+}
 
 function getCanvas(rows, columns) {
     const pixelSize = 640/rows;
-
+    clearCanvas();
     for (let i = 0; i < rows*columns; i++) {
         const divPixel = document.createElement('div')
         divPixel.setAttribute('style', `height:${pixelSize}px; width:${pixelSize}px`)
@@ -17,28 +29,8 @@ function getCanvas(rows, columns) {
         canvas.appendChild(divPixel)
     }
 }
-getCanvas(50, 50);
-
-// slider.oninput = () => {
-//     let val =
-// }
-
-function myFunction() {
-    // const canvas = document.querySelector('.canvas')
-    // canvas.innerHTML = "";
-
-    clearCanvas() 
-    const slider = document.querySelector('#myRange');
-    const gridSizeText = document.querySelector('.hello-text');
-    gridSizeText.textContent = `${slider.value}x${slider.value}`;
-    getCanvas(slider.value, slider.value)
-  }
 
 function clearCanvas() {
-    const pixels = document.querySelectorAll('.pixel');
-    const canvas = document.querySelector('.canvas')
-
-    for (pixel of pixels) {
-        canvas.removeChild(pixel);
-    }
+    const canvas = document.querySelector('.canvas');
+    canvas.innerHTML = "";
 }
